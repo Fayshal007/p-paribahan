7626720c33c763f058865b3b333d853abbe9e1d1
Fayshal007 <fayshalislam5@gmail.com>
2024-04-28 15:55:15 +0600

    selected seat list done

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const selectedSeatCountElement = document.getElementById('selected-seat-count');
+const selectedSeatList = document.getElementById('selected-seat-list');
 
-document.querySelectorAll('.seat-table tbody tr td button').forEach(function(button) {
-    button.addEventListener('click', function(event) {
+document.querySelectorAll('.seat-table tbody tr td button').forEach(function (button) {
+    button.addEventListener('click', function (event) {
+        // Get the text of the parent <td> element (seat number)
+        var seatNumber = event.target.parentElement.textContent.trim();
+        // Log the seat number
+        console.log(seatNumber);
+        // Or do whatever you want with the seat number
+        const newSeattr = document.createElement('tr');
+        newSeattr.innerHTML = `
+                <td>${seatNumber}</td>
+                <td>Economy</td>
+                <td>550</td>
+                `
         // Check if the seat is already selected
         if (event.target.classList.contains('selected')) {
             // If the seat is already selected, remove the selected class
@@ -21,20 +33,19 @@ document.querySelectorAll('.seat-table tbody tr td button').forEach(function(but
                 event.target.style.color = 'white';
                 // Decrease the total seat count by 1
                 decreaseTotalSeatCount();
-                let selectedSeatCount =  parseInt(selectedSeatCountElement.innerText);
+                let selectedSeatCount = parseInt(selectedSeatCountElement.innerText);
                 selectedSeatCount += 1;
                 selectedSeatCountElement.innerText = selectedSeatCount;
+                const firstROw = selectedSeatList.firstChild;
+                selectedSeatList.insertBefore(newSeattr, firstROw)
+
             } else {
                 // If the maximum number of seats has been reached, alert the user
                 alert('You can only select up to 4 seats.');
             }
         }
 
-        // Get the text of the parent <td> element (seat number)
-        var seatNumber = event.target.parentElement.textContent.trim();
-        // Log the seat number
-        console.log(seatNumber);
-        // Or do whatever you want with the seat number
+
     });
 });
 
